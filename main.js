4ef8c8f7199d33b65b5838466be8f68b18b0aceb
DESKTOP-5I81B46\Chris <chrisjfoss@gmail.com>
2018-06-23 11:26:37 -0400

    Added quick start application as starting point, with some changes I made to help learn the framework.

---
+++ main.js
@@ -0,0 +1,114 @@
+const {app, BrowserWindow, ipcMain, Menu, MenuItem} = require('electron')
+const url = require('url')
+const path = require('path')
+
+let win
+
+function createWindow() {
+   win = new BrowserWindow({width: 800, height: 600})
+   win.loadURL(url.format ({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+   }))
+}
+
+//PASSING VARIABLES BETWEEN PROCESSES, HANDLE THE "HEAVY WORK" HERE
+// Event handler for asynchronous incoming messages
+ipcMain.on('asynchronous-message', (event, arg) => {
+   console.log(arg)
+
+   // Event emitter for sending asynchronous messages
+   event.sender.send('asynchronous-reply', 'async pong')
+})
+
+// Event handler for synchronous incoming messages
+ipcMain.on('synchronous-message', (event, arg) => {
+   console.log(arg)
+
+   // Synchronous event emmision
+   event.returnValue = 'sync pong'
+})
+
+//CREATE MENUS
+const template = [
+   {
+      label: 'Edit',
+      submenu: [
+         {
+            role: 'undo'
+         },
+         {
+            role: 'redo'
+         },
+         {
+            type: 'separator'
+         },
+         {
+            role: 'cut'
+         },
+         {
+            role: 'copy'
+         },
+         {
+            role: 'paste'
+         }
+      ]
+   },
+
+   {
+      label: 'View',
+      submenu: [
+         {
+            role: 'reload'
+         },
+         {
+            role: 'toggledevtools'
+         },
+         {
+            type: 'separator'
+         },
+         {
+            role: 'resetzoom'
+         },
+         {
+            role: 'zoomin'
+         },
+         {
+            role: 'zoomout'
+         },
+         {
+            type: 'separator'
+         },
+         {
+            role: 'togglefullscreen'
+         }
+      ]
+   },
+
+   {
+      role: 'window',
+      submenu: [
+         {
+            role: 'minimize'
+         },
+         {
+            role: 'close'
+         }
+      ]
+   },
+
+   {
+      role: 'help',
+      submenu: [
+         {
+            label: 'Learn More'
+         }
+      ]
+   }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
+
+app.on('ready', createWindow)
